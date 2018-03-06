cd4191b9441892af594252aa8eb66a8c62d7ebbd
Chris Lei <chris@divbzero.com>
2018-03-06 01:35:13 -0800

    {GET => OPTIONS|HEAD|GET|POST|PUT|PATCH|DELETE}

--- aioclient.py
+++ aioclient.py
@@ -1,8 +1,11 @@
+import io
+from xml.etree import ElementTree
+
 from aiohttp import ClientSession
 
-async def get(*args, **kargs):
+async def request(method, *args, **kargs):
     async with ClientSession() as session:
-        async with session.get(*args, **kargs) as response:
+        async with session.request(method, *args, **kargs) as response:
             mimetype = response.content_type
             if mimetype.endswith('json'):
                 return response.status, response.headers, await response.json()
@@ -10,3 +13,24 @@ async def get(*args, **kargs):
                 return response.status, response.headers, await response.text()
             else:
                 return response.status, response.headers, await response.read()
+
+async def options(*args, **kargs):
+    return await request('OPTIONS', *args, **kargs)
+
+async def head(*args, **kargs):
+    return await request('HEAD', *args, **kargs)
+
+async def get(*args, **kargs):
+    return await request('GET', *args, **kargs)
+
+async def post(*args, **kargs):
+    return await request('POST', *args, **kargs)
+
+async def put(*args, **kargs):
+    return await request('PUT', *args, **kargs)
+
+async def patch(*args, **kargs):
+    return await request('PATCH', *args, **kargs)
+
+async def delete(*args, **kargs):
+    return await request('DELETE', *args, **kargs)
